7ff185860dd8aa48af2d6cc85d919187a8682399
Muhammad Awais <MuhammadAwais3819@gmail.com>
2024-12-03 22:05:33 +0500

    update all new changes to form validations

--- src/pages/auth/create-account.jsx
+++ src/pages/auth/create-account.jsx
@@ -1,97 +1,204 @@
 "use client";
+import Input from "@/components/Input";
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
 import StyledImage from "@/components/StyedImage";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+  email: yup.string().required("e-mail is required"),
+  password: yup.string().required("password is required"),
+  cPassword: yup.string().required("confirm password is required"),
+});
+
+const useYupValidationResolver = (validationSchema) =>
+  useCallback(
+    async (data) => {
+      try {
+        const values = await validationSchema.validate(data, {
+          abortEarly: false,
+        });
+
+        return {
+          values,
+          errors: {},
+        };
+      } catch (errors) {
+        return {
+          values: {},
+          errors: errors.inner.reduce(
+            (allErrors, currentError) => ({
+              ...allErrors,
+              [currentError.path]: {
+                type: currentError.type ?? "validation",
+                message: currentError.message,
+              },
+            }),
+            {}
+          ),
+        };
+      }
+    },
+    [validationSchema]
+  );
 
 export default function CreateAccount() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const router = useRouter();
+  const resolver = useYupValidationResolver(validationSchema);
+
+  const {
+    handleSubmit,
+    register,
+    control,
+    reset,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver,
+  });
+
+  function handleFormSubmit(value) {
+    console.log(value);
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen max-h-screen xl:justify-between gap-10 w-full mx-auto ">
-      <div className="w-full max-w-[50%] flex items-center justify-center ">
-        <div className="w-full max-w-[586px] pl-6 ">
-          <p className="text-30 font-quickSand font-bold text-center ">
-            Crear Cuenta
-          </p>
-          <p className="mt-6 font-quickSand font-bold">Email</p>
-          <div className="bg-light-brand-200 py-2 px-3 mt-2 rounded-[10px] border border-grayTwo text-light-brand-300 w-full ">
-            <input
-              type="password"
-              className="bg-[transparent] w-full text-16 outline-none "
-              placeholder="Ejemplo@coinestate.com.co"
-            />
-          </div>
-          <div className="text-lightGray-700">
-            <div className="flex items-center justify-between mt-6 w-full">
-              <p className="font-quickSand font-bold ">Password</p>
-            </div>
-            <div className="bg-light-brand-200 py-2 px-3 mt-2 rounded-[10px] border border-grayTwo text-light-brand-300 flex items-center justify-between ">
-              <input
-                type={showPassword ? "text" : "password"}
-                className="bg-[transparent] w-full text-16 outline-none "
-                placeholder="*****************"
+      <form
+        className="w-full flex justify-center "
+        onSubmit={handleSubmit((data) => {
+          handleFormSubmit(data);
+        })}
+      >
+        <div className="w-full max-w-[50%] flex items-center justify-center ">
+          <div className="w-full max-w-[586px] pl-6 ">
+            <p className="text-30 font-quickSand font-bold text-center ">
+              Crear Cuenta
+            </p>
+            <p className="mt-6 font-quickSand font-bold">Email</p>
+            <div
+              className={`bg-light-brand-200 ${
+                errors?.email?.message
+                  ? "border-2 border-red-300"
+                  : "border border-grayTwo"
+              } px-3 mt-2 rounded-[10px] text-light-brand-300 flex items-center justify-between `}
+            >
+              <Input
+                className={"border-none text-black-100 "}
+                placeholder="Example@gmail.com"
+                type="text"
+                // error={errors?.password}
+                register={register("email")}
               />
-              <button onClick={() => setShowPassword(!showPassword)}>
-                {showPassword === true ? (
-                  <StyledImage
-                    src="/assets/svg/EyeIcon.svg"
-                    className="w-4 h-4  "
+            </div>
+            {errors?.email?.message && (
+              <span className="text-[red] text-xs">
+                {errors?.email?.message}
+              </span>
+            )}
+            <div className="text-lightGray-700">
+              <div className="mt-4 ">
+                <div className="flex items-center justify-between w-full text-lightGray-700 ">
+                  <p className="font-bold font-quickSand ">Password:</p>
+                </div>
+                <div
+                  className={`bg-light-brand-200 ${
+                    errors?.password?.message
+                      ? "border-2 border-red-300"
+                      : "border border-grayTwo"
+                  } px-3 mt-2 rounded-[10px] text-light-brand-300 flex items-center justify-between `}
+                >
+                  <Input
+                    className={"border-none text-black-100 "}
+                    placeholder="****"
+                    type={showPassword === true ? "text" : "password"}
+                    // error={errors?.password}
+                    register={register("password")}
                   />
-                ) : (
-                  <StyledImage
-                    src="/assets/svg/HideEyeIcon.svg"
-                    className="w-4 h-4  "
-                  />
+
+                  <button onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword === true ? (
+                      <StyledImage
+                        src="/assets/svg/EyeIcon.svg"
+                        className="w-4 h-4  "
+                      />
+                    ) : (
+                      <StyledImage
+                        src="/assets/svg/HideEyeIcon.svg"
+                        className="w-4 h-4  "
+                      />
+                    )}
+                  </button>
+                </div>
+                {errors?.password?.message && (
+                  <span className="text-[red] text-xs">
+                    {errors?.password?.message}
+                  </span>
                 )}
-              </button>
-            </div>
-            <div className="flex items-center justify-between mt-6 ">
-              <p className="font-quickSand font-bold">Confirm Password</p>
-            </div>
-            <div className="bg-light-brand-200 border border-grayTwo py-2 px-3 mt-2 rounded-[10px] text-light-brand-300 flex items-center justify-between ">
-              <input
-                type={showConfirmPassword ? "text" : "password"}
-                className="bg-[transparent] w-full text-16 outline-none "
-                placeholder="*****************"
-              />
-              <button
-                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              </div>
+              <div className="flex items-center justify-between mt-6 ">
+                <p className="font-quickSand font-bold">Confirm Password</p>
+              </div>
+              <div
+                className={`bg-light-brand-200 ${
+                  errors?.cPassword?.message
+                    ? "border-2 border-red-300"
+                    : "border border-grayTwo"
+                } px-3 mt-2 rounded-[10px] text-light-brand-300 flex items-center justify-between `}
               >
-                {showConfirmPassword === true ? (
-                  <StyledImage
-                    src="/assets/svg/EyeIcon.svg"
-                    className="w-4 h-4  "
-                  />
-                ) : (
-                  <StyledImage
-                    src="/assets/svg/HideEyeIcon.svg"
-                    className="w-4 h-4  "
-                  />
-                )}
-              </button>
+                <Input
+                  className={"border-none text-black-100 "}
+                  placeholder="****"
+                  type={showConfirmPassword === true ? "text" : "password"}
+                  // error={errors?.password}
+                  register={register("cPassword")}
+                />
+                <button
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                  {showConfirmPassword === true ? (
+                    <StyledImage
+                      src="/assets/svg/EyeIcon.svg"
+                      className="w-4 h-4  "
+                    />
+                  ) : (
+                    <StyledImage
+                      src="/assets/svg/HideEyeIcon.svg"
+                      className="w-4 h-4  "
+                    />
+                  )}
+                </button>
+              </div>
+              {errors?.cPassword?.message && (
+                <span className="text-[red] text-xs">
+                  {errors?.cPassword?.message}
+                </span>
+              )}
             </div>
+            <button
+              type="submit"
+              onClick={() => router.push("/dashboard/dashboard-wallet")}
+              className="bg-Yellow-100 p-3 text-lightGray-700 font-bold font-quickSand mt-20 w-full rounded-[10px] "
+            >
+              Create Your Account
+            </button>
+            <p className="text-lightGray-700 font-semibold mt-14 text-center ">
+              Have you already registered?
+            </p>
+            <p className="underline text-blue-500 mt-7 text-center ">Log In</p>
           </div>
-          <button
-            onClick={() => router.push("/dashboard/dashboard-wallet")}
-            className="bg-Yellow-100 p-3 text-lightGray-700 font-bold font-quickSand mt-20 w-full rounded-[10px] "
-          >
-            Create Your Account
-          </button>
-          <p className="text-lightGray-700 font-semibold mt-14 text-center ">
-            Have you already registered?
-          </p>
-          <p className="underline text-blue-500 mt-7 text-center ">Log In</p>
         </div>
-      </div>
-      <div>
-        <img
-          src="/assets/images/CreateAccountMainImg.png"
-          className="w-full min-h-screen max-h-screen hidden xl:block "
-        />
-      </div>
+        <div>
+          <img
+            src="/assets/images/CreateAccountMainImg.png"
+            className="w-full min-h-screen max-h-screen hidden xl:block "
+          />
+        </div>
+      </form>
     </div>
   );
 }
--- src/pages/auth/log-in.jsx
+++ src/pages/auth/log-in.jsx
@@ -2,11 +2,19 @@
 "use client";
 import Input from "@/components/Input";
 import StyledImage from "@/components/StyedImage";
-import React, { useState } from "react";
+import {
+  useMutateCreateBlog,
+  useMutateCreateProperty,
+  useMutateUploadFiles,
+  useMutateUploadMultiFiles,
+} from "@/hooks/mutation";
+import React, { useCallback, useState } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
+import * as yup from "yup";
 
 const validationSchema = yup.object({
-  email: yup.number().required("E-Mail is required"),
-  password: yup.number().required("Password is required"),
+  email: yup.string().required("e-mail is required"),
+  password: yup.string().required("password is required"),
 });
 
 const useYupValidationResolver = (validationSchema) =>
@@ -41,108 +49,118 @@ const useYupValidationResolver = (validationSchema) =>
   );
 
 export default function LogIn() {
-  const [showEmail, setShowEmail] = useState(true);
   const [showPassword, setShowPassword] = useState(true);
   const resolver = useYupValidationResolver(validationSchema);
-  const {
-    mutate: mutateUploadMainFile,
-    data: mainImageData,
-    isPending: isLoadingMain,
-  } = useMutateUploadFiles();
-  const {
-    mutate: mutateMultiImages,
-    isPending: isUploadingMultiFiles,
-    data: multiFilesList,
-  } = useMutateUploadMultiFiles();
-  const { mutate: createProperty, isPending: isLoadingCreateNfts } =
-    useMutateCreateProperty();
 
   const {
     handleSubmit,
     register,
     control,
     reset,
+    setValue,
     formState: { errors },
   } = useForm({
     resolver,
   });
 
   function handleFormSubmit(value) {
-    // if (mainImageData?.IpfsHash && multiFilesList?.length > 0) {
-    const defaultValues = {
-      ...value,
-      image:
-        mainImageData?.IpfsHash ||
-        "QmTsTAMASbvwfGUxhqdV48h8HG55v3PT4eGVjLYQ8SCE1R",
-      subImages: multiFilesList || [
-        "QmVVEGcA8S7k5ewTdEf33hXnecQYT3YRTyH828VrJ7YwZU",
-      ],
-      email: "demo@gmail.com",
-    };
-    createProperty(defaultValues);
-    // }else{
-    // toast.error('data is missing')
-    // }
+    console.log(value);
   }
+
+ 
 
   const step = "0.001";
   return (
     <div className="bg-black-800 min-h-screen flex items-center justify-center font-inter ">
-      <div className="bg-white p-6 rounded-[20px] w-full max-w-[500px]  ">
-        <p className="text-30 font-bold text-center">LogIn</p>
-        <div className="mt-4">
-          <p className="font-bold font-quickSand">Email:</p>
-          <div className="bg-light-brand-200 border border-grayTwo py-2 px-3 mt-2 rounded-[10px] text-light-brand-300 flex items-center justify-between ">
-            <input
-              type={"email"}
-              className="bg-[transparent] w-full text-16 outline-none "
-              placeholder="Ejemplo@coinestate.com.co"
-            />
+      <form
+        className="w-full flex justify-center "
+        onSubmit={handleSubmit((data) => {
+          handleFormSubmit(data);
+        })}
+      >
+        <div className="bg-white p-6 rounded-[20px] w-full max-w-[500px]  ">
+          <p className="text-30 font-bold text-center">LogIn</p>
+          <div className="mt-4">
+            <p className="font-bold font-quickSand">Email:</p>
+            <div
+              className={`bg-light-brand-200 ${
+                errors?.password?.message
+                  ? "border-2 border-red-300"
+                  : "border border-grayTwo"
+              }px-3 mt-2 rounded-[10px] w-full  text-light-brand-300 flex items-center justify-between `}
+            >
+              <Input
+                className={"border-none text-black-100 "}
+                placeholder="Example@gmail.com"
+                type="text"
+                // error={errors?.password}
+                register={register("email")}
+              />
+            </div>
+            {errors?.email?.message && (
+              <span className="text-[red] text-xs">
+                {errors?.email?.message}
+              </span>
+            )}
           </div>
-        </div>
-        <div className="mt-4 ">
-          <div className="flex items-center justify-between w-full text-lightGray-700 ">
-            <p className="font-bold font-quickSand ">Password:</p>
-            {/* <button
+          <div className="mt-4 ">
+            <div className="flex items-center justify-between w-full text-lightGray-700 ">
+              <p className="font-bold font-quickSand ">Password:</p>
+              {/* <button
               className="font-quickSand font-bold "
               onClick={() => setShowPassword(!showPassword)}
             >
               {showPassword ? "Hide" : "Show"}
             </button> */}
+            </div>
+            <div
+              className={`bg-light-brand-200 ${
+                errors?.password?.message
+                  ? "border-2 border-red-300"
+                  : "border border-grayTwo"
+              } px-3 mt-2 rounded-[10px] text-light-brand-300 flex items-center justify-between `}
+            >
+              <Input
+                className={"border-none text-black-100 "}
+                placeholder="****"
+                type={showPassword === true ? "text" : "password"}
+                // error={errors?.password}
+                register={register("password")}
+              />
+
+              <button onClick={() => setShowPassword(!showPassword)}>
+                {showPassword === true ? (
+                  <StyledImage
+                    src="/assets/svg/EyeIcon.svg"
+                    className="w-4 h-4  "
+                  />
+                ) : (
+                  <StyledImage
+                    src="/assets/svg/HideEyeIcon.svg"
+                    className="w-4 h-4  "
+                  />
+                )}
+              </button>
+            </div>
+            {errors?.password?.message && (
+              <span className="text-[red] text-xs">
+                {errors?.password?.message}
+              </span>
+            )}
           </div>
-          <div className="bg-light-brand-200 border border-grayTwo py-2 px-3 mt-2 rounded-[10px] text-light-brand-300 flex items-center justify-between ">
-            <input
-              type={showPassword === true ? "text" : "password"}
-              className="bg-[transparent]  w-full text-16 outline-none "
-              placeholder="****"
-            />
-            <button onClick={() => setShowPassword(!showPassword)}>
-              {showEmail === true ? (
-                <StyledImage
-                  src="/assets/svg/EyeIcon.svg"
-                  className="w-4 h-4  "
-                />
-              ) : (
-                <StyledImage
-                  src="/assets/svg/HideEyeIcon.svg"
-                  className="w-4 h-4  "
-                />
-              )}
-            </button>
+          <button className="text-lightGray-700 w-full bg-Yellow-100 rounded-[10px] mt-6 p-3 text-20 font-bold ">
+            Log In
+          </button>
+          {/* <hr className="my-4 p-1 border-none bg-grayTwo rounded-[100%] " /> */}
+          <p className="text-lightGray-700 font-bold font-quickSand mt-8 ">
+            Don't have an account yet?
+          </p>
+          <div className="flex items-center justify-between mt-4 px-6 text-blue-500 font-medium underline ">
+            <button type="submit">Sign up</button>
+            <button>Forgot your password?</button>
           </div>
         </div>
-        <button className="text-lightGray-700 w-full bg-Yellow-100 rounded-[10px] mt-6 p-3 text-20 font-bold ">
-          Log In
-        </button>
-        {/* <hr className="my-4 p-1 border-none bg-grayTwo rounded-[100%] " /> */}
-        <p className="text-lightGray-700 font-bold font-quickSand mt-8 ">
-          Don't have an account yet?
-        </p>
-        <div className="flex items-center justify-between mt-4 px-6 text-blue-500 font-medium underline ">
-          <button type="submit">Sign up</button>
-          <button>Forgot your password?</button>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
